df0ef2094460d884a611ac3d89c54a4f4580f462
arusalq <ach.rusdhaini@gmail.com>
2023-09-02 10:12:40 +0700

    server create routes, auth controller, and User Model

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 // CONFIGURATION
 const __filename = fileURLToPath(import.meta.url);
@@ -34,11 +35,14 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage });
 
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register);
+
 // MONGOOSE SETUP
-console.log(process.env.PORT);
-const PORT = process.env.NODE_PORT || 6001;
+// console.log(process.env.PORT);
+const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.NODE_MONGO_URL, {
-    useNewUrlParser :true ,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server port ${PORT}`));
