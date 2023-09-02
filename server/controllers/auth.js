a5e8afcc0090905c3bffa89c5ba69363aef1eb0d
arusalq <ach.rusdhaini@gmail.com>
2023-09-02 12:26:40 +0700

    create aut routes, login controller and middleware

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,7 +4,6 @@ import User from '../models/User.js';
 
 // REGISTER USER
 export const register = async (req, res) => {
-    console.log('test');
     try {
         const {
             firstName,
@@ -35,6 +34,25 @@ export const register = async (req, res) => {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
 
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+// LOGGING IN
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "user does not exist." });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "invalid credentials," });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+        delete user.password;
+
+        res.status(200).json({ token, user });
     } catch (err) {
         res.status(500).json({ error: err.message })
     }
