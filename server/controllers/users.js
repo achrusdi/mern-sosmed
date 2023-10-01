7819f9c6e7b7bdf55be65880002c9f36cba22ddb
arusalq <ach.rusdhaini@gmail.com>
2023-10-02 01:33:11 +0700

    server fix error add friend

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -31,7 +31,7 @@ export const addRemoveFriend = async (req, res) => {
     try {
         const { id, friendId } = req.params;
         const user = await User.findById(id);
-        const friend = await User.findById(friendById);
+        const friend = await User.findById(friendId);
 
         if (user.friends.includes(friendId)) {
             user.friends = user.friends.filter((id) => id !== friendId);
