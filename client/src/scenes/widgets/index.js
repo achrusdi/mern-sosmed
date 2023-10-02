a3c73590042fe38f0878aff0bba643e37418c3e3
arusalq <ach.rusdhaini@gmail.com>
2023-10-02 21:46:10 +0700

    client create advert dan friend list widget

--- client/src/scenes/widgets/index.js
+++ client/src/scenes/widgets/index.js
@@ -2,5 +2,7 @@ import UserWidget from "./UserWidget";
 import MyPostWidget from "./MyPostWidget";
 import PostWidget from "./PostWidget";
 import PostsWidget from "./PostsWidget";
+import AdvertWidget from "./AdvertWidget";
+import FriendListWidget from "./FriendListWidget";
 
-export { UserWidget, MyPostWidget, PostWidget, PostsWidget };
+export { UserWidget, MyPostWidget, PostWidget, PostsWidget, AdvertWidget, FriendListWidget };
