92bd9c6fbe211e2b578625b4d2a34f57ca4ee48d
arusalq <ach.rusdhaini@gmail.com>
2023-10-02 21:03:56 +0700

    client the add and remove buttons do not appear if the post owner matches the person's login ID

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -21,8 +21,6 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
   const isFriend = friends.find((friend) => friend._id === friendId);
 
   const patchFriend = async () => {
-    console.log(_id);
-    console.log(friendId);
     const response = await fetch(
       `http://localhost:3001/users/${_id}/${friendId}`,
       {
@@ -65,16 +63,18 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
           </Typography>
         </Box>
       </FlexBetween>
-      <IconButton
-        onClick={() => patchFriend()}
-        sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
-      >
-        {isFriend ? (
-          <PersonRemoveOutlined sx={{ color: primaryDark }} />
-        ) : (
-          <PersonAddOutlined sx={{ color: primaryDark }} />
-        )}
-      </IconButton>
+      {_id !== friendId && (
+        <IconButton
+          onClick={() => patchFriend()}
+          sx={{ backgroundColor: primaryLight, p: "0.6rem" }}
+        >
+          {isFriend ? (
+            <PersonRemoveOutlined sx={{ color: primaryDark }} />
+          ) : (
+            <PersonAddOutlined sx={{ color: primaryDark }} />
+          )}
+        </IconButton>
+      )}
     </FlexBetween>
   );
 };
